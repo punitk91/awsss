68316dc2ab727a7009e636e4a814233c68ec178d
punitk91 <pkulkarni290@gmail.com>
2024-02-21 21:12:25 +0530

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "hello aws"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+print("hello")
