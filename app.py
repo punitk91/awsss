f34fa48bcc0de70435d2a27bef1a1d7afcf56f59
punitk91 <pkulkarni290@gmail.com>
2024-02-22 08:41:12 +0530

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("hello")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "hello world"
+
+if __name__ == '__main__':
+    app.run(debug=True)
